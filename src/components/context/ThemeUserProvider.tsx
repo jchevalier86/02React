a44bc19c92340945087ba834469863cb5205025e
Johnny Chevalier <johnny.chevalier91@gmail.com>
2024-06-28 14:49:26 +0200

    modif

--- src/components/context/ThemeUserProvider.tsx
+++ src/components/context/ThemeUserProvider.tsx
@@ -1,21 +1,30 @@
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode, useReducer } from "react";
+import IState from "../../interfaces/reducer/IState";
+import reducerTraitement from "../../reducers/reducerTraitement";
 
 // objet user de type useState qu'on partage
 interface IUserContextProps {
   user: string;
   setUser: React.Dispatch<React.SetStateAction<string>>;
+  civilite?: string;
 }
 
 // le user context qui permet de récupérer les données dans les composants enfants
 export const UserContext = createContext<IUserContextProps | undefined>(
   undefined
 );
+const initialState: IState = { count: 0, civilite: [] };
 
 export const ThemeUserProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   // initialise le useState
   const [user, setUser] = useState("bob");
+
+  const [state, dispatch] = useReducer(reducerTraitement, initialState);
+  dispatch({ type: "civilite" });
+  let civilite = state.civilite;
+  // {civilite:["M", "MME", "Autre"]}
 
   //créé Provider à partir du UserContext
   return (
