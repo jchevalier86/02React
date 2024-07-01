4ad5ac0bcecf820fd58e2624012c865a97c856b6
Johnny Chevalier <johnny.chevalier91@gmail.com>
2024-07-01 10:07:49 +0200

    modif

--- src/components/UseMemoV1.tsx
+++ src/components/UseMemoV1.tsx
@@ -1,24 +1,57 @@
-import React, { useState, useEffect, useMemo } from "react";
+import { useState, useMemo, useEffect, useRef } from "react";
 
+let counter = 0;
 const UseMemoV1: React.FC<any> = () => {
+  const rendersNo = useRef(0);
+
+  rendersNo.current = rendersNo.current + 1;
+  const [tva, setTva] = useState(0);
+  const [nbre, setNbre] = useState(0);
+  const [resultat, setResultat] = useState(0);
+
+  // time is a state variable that changes once per second,
+  // so that it's always in sync with the current time.
   const time = useTime();
-}
 
-function useTime() {
-  const [time, setTime] = useState(new Date());
+  function useTime() {
+    const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    const intervalId = window.setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    useEffect(() => {
+      const intervalId = window.setInterval(() => {
+        setTime(new Date());
+      }, 1000);
 
-    return () => {
-      window.clearInterval(intervalId);
-    };
-  }, []);
+      return () => {
+        window.clearInterval(intervalId);
+      };
+    }, []);
 
-  return time; (
-    {time! ? time.toLocaleTimeString() : ""}
+    return time;
+  }
+  //on vérifie si un des deux states a changer
+  const calculer = useMemo(() => {
+    setResultat(nbre * tva);
+  }, [tva]);
+
+  console.log("counting re-renders v4", rendersNo.current);
+  return (
+    <div>
+      <>
+        <p className="clock">{time! ? time.toLocaleTimeString() : ""}</p>
+        <label htmlFor="num">Your number:</label>
+        <input
+          type="number"
+          value={tva}
+          onChange={(e) => setTva(Number(e.target.value))}
+        ></input>
+        <input
+          type="number"
+          value={nbre}
+          onChange={(e) => setNbre(Number(e.target.value))}
+        ></input>
+        <button onClick={() => calculer}>calculer</button>Résultat {resultat}
+      </>
+    </div>
   );
 };
 
